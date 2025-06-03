e387243fe037ead3262d017c014901f52933e11f
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-06-03 09:00:50 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 echo -e "\e[1;36m+--------------------------------------------------+\e[0m"
 echo -e "\e[1;36m|   Install Bot Manager DigitalOcean By Sanstore   |\e[0m"
 echo -e "\e[1;36m+--------------------------------------------------+\e[0m"
-read -p "Masukkan Token BOT : "
-read -p "Masukkan ChatID : "
+read -p "Masukkan Token BOT : " bottoken
+read -p "Masukkan ChatID : " chatid
+
+sed -i "s/TOKEN_BOT/${bottoken}/" /opt/do-manager/do-manager.py
+sed -i "s/CHATID/${chatid}/" /opt/do-manager/do-manager.py
