92ee863a2bcf94d9d479c693c8e695f5e1612a02
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-06-03 09:08:34 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,72 @@ read -p "Masukkan ChatID : " chatid
 mkdir -p /opt/do-manager/
 cd /opt/do-manager/
 
-wget -q https://raw.githubusercontent.com/Sandhj/tools/main/do-manager.py
+# Buat venv untuk Bot
+# Fungsi untuk deteksi OS dan versi
+OS=$(grep -Ei '^(NAME|VERSION_ID)=' /etc/os-release | cut -d= -f2 | tr -d '"')
 
+# Ekstrak nama distro dan versi
+DISTRO=$(echo "$OS" | head -n1)
+VERSION=$(echo "$OS" | tail -n1)
+
+# Cek apakah Debian 12
+if [[ "$DISTRO" == "Debian GNU/Linux" && "$VERSION" == "12" ]]; then
+    echo "OS Terdeteksi: Debian 12"
+    apt update && apt install python3.11-venv -y
+
+# Cek apakah Ubuntu 24.04 LTS
+elif [[ "$DISTRO" == "Ubuntu" && "$VERSION" == "24.04" ]]; then
+    echo "OS Terdeteksi: Ubuntu 24.04 LTS"
+    apt update && apt install python3.12-venv -y
+
+# Jika bukan salah satu dari di atas
+else
+    echo "OS Tidak Didukung!"
+    echo "Distro: $DISTRO"
+    echo "Versi: $VERSION"
+    exit 1
+fi
+
+
+python3 -m venv bot
+source bot/bin/activate
+
+apt-get install -y python3-pip
+
+# Instal modul Python yang diperlukan
+pip3 install requests
+pip3 install schedule
+pip3 install pyTelegramBotAPI
+
+cat <<EOL > run.sh
+#!/bin/bash
+source /opt/do-manager/bot/bin/activate
+python3 /opt/do-manager/do-manager.py
+EOL
+
+wget -q https://raw.githubusercontent.com/Sandhj/tools/main/do-manager.py
 sed -i "s/TOKEN_BOT/${bottoken}/" /opt/do-manager/do-manager.py
 sed -i "s/CHATID/${chatid}/" /opt/do-manager/do-manager.py
+
+# Buat file service systemd
+cat <<EOF > /etc/systemd/system/do-manager.service
+[Unit]
+Description=Do Manager Bot Service
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/bash /opt/do-manager/run.sh
+WorkingDirectory=/opt/do-manager
+StandardOutput=inherit
+StandardError=inherit
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd dan mulai service
+systemctl daemon-reload
+systemctl enable do-manager
+systemctl start do-manager
