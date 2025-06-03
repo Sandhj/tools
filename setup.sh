19dae950f3ac8bbb410b1721420a719caaa34401
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-06-03 09:03:26 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,10 @@ echo -e "\e[1;36m+--------------------------------------------------+\e[0m"
 read -p "Masukkan Token BOT : " bottoken
 read -p "Masukkan ChatID : " chatid
 
+mkdir -p /opt/do-manager/
+cd /opt/do-manager/
+
+wget -q https://raw.githubusercontent.com/Sandhj/tools/main/do-manager.py
+
 sed -i "s/TOKEN_BOT/${bottoken}/" /opt/do-manager/do-manager.py
 sed -i "s/CHATID/${chatid}/" /opt/do-manager/do-manager.py
